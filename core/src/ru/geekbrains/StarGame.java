188c44e11dbab6d20ca1857963eff69d40979905
Grigoriy <arcebus84@yandex.ru>
2020-02-16 23:38:00 +0300

    Lesson1 orientation changed to portrait; new background was added.

--- core/src/ru/geekbrains/StarGame.java
+++ core/src/ru/geekbrains/StarGame.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class StarGame extends ApplicationAdapter {
+
 	private SpriteBatch batch;
 	private Texture background;
 	
