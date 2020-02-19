8a15feae39559bcfc68d786f5d43c4a0c655a9bf
Grigoriy <arcebus84@yandex.ru>
2020-02-19 22:25:08 +0300

    Lesson2 orientation changed to portrait; new background was added.

--- core/src/ru/geekbrains/StarGame.java
+++ core/src/ru/geekbrains/StarGame.java
@@ -1,34 +1,12 @@
 package ru.geekbrains;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Game;
+import ru.geekbrains.screen.MenuScreen;
 
-public class StarGame extends ApplicationAdapter {
-
-	private SpriteBatch batch;
-	private Texture background;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		background = new Texture("planet.jpg");
-	}
+public class StarGame extends Game {
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(background, 0, 0,  Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		background.dispose();
+	public void create() {
+		setScreen(new MenuScreen());
 	}
 }
