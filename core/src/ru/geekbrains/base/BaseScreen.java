a4048efb966c2dfded54b44ce9c92f46e224a98c
Grigoriy <arcebus84@yandex.ru>
2020-02-29 21:59:42 +0300

    Lesson 3

--- core/src/ru/geekbrains/base/BaseScreen.java
+++ core/src/ru/geekbrains/base/BaseScreen.java
@@ -4,15 +4,40 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Matrix3;
+import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.math.Vector2;
+
+import ru.geekbrains.math.MatrixUtils;
+import ru.geekbrains.math.Rect;
 
 public class BaseScreen implements Screen, InputProcessor {
 
     protected SpriteBatch batch;
 
+    private Rect screenBounds;      // координаты в пикселях
+    private Rect worldBounds;       // наша координатная сетка
+    private Rect glBounds;          // координаты OpenGL
+
+    private Matrix4 worldToGl;      // transform world map to OpenGL map
+    private Matrix3 screenToWorld;  // transform screen map to world map
+
+    private Vector2 touch;          // вектор нажатия во внутренних координатах
+
     @Override
     public void show() {
         System.out.println("show");
         batch = new SpriteBatch();
+
+        screenBounds  = new Rect();
+        worldBounds   = new Rect();
+        glBounds      = new Rect(0, 0, 1f, 1f);
+
+        touch         = new Vector2();
+
+        worldToGl     = new Matrix4();
+        screenToWorld = new Matrix3();
+
         Gdx.input.setInputProcessor(this);
     }
 
@@ -23,7 +48,26 @@ public class BaseScreen implements Screen, InputProcessor {
 
     @Override
     public void resize(int width, int height) {
+        // выполняется после инициализации
         System.out.println("resize width = " + width + " height = " + height);
+        screenBounds.setSize(width, height);
+        screenBounds.setLeft(0);
+        screenBounds.setBottom(0);
+
+        worldBounds.setHeight(1f);
+        float aspect = width / (float) height;
+        worldBounds.setWidth(1f * aspect);
+
+        MatrixUtils.calcTransitionMatrix(worldToGl, worldBounds, glBounds);
+        MatrixUtils.calcTransitionMatrix(screenToWorld, screenBounds, worldBounds);
+
+        batch.setProjectionMatrix(worldToGl);
+
+        resize(worldBounds);
+    }
+
+    public void resize(Rect worldBounds) {
+        System.out.println("worldBounds width = " + worldBounds.getHalfWidth() + " height = " + worldBounds.getHeight());
     }
 
     @Override
@@ -69,8 +113,17 @@ public class BaseScreen implements Screen, InputProcessor {
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
         System.out.println("touchDown screenX = " + screenX + " screenY = " + screenY);
+        touch.set(screenX, screenBounds.getHeight() - screenY).mul(screenToWorld);
+        touchDown(touch, pointer, button);
         return false;
     }
+
+    public boolean touchDown(Vector2 touch, int pointer, int button) {
+        System.out.println("touchDown vector world = " + touch.x + " screenY = " + touch.y);
+
+        return false;
+    }
+
 
     @Override
     public boolean touchUp(int screenX, int screenY, int pointer, int button) {
@@ -94,4 +147,5 @@ public class BaseScreen implements Screen, InputProcessor {
         System.out.println("scrolled amount = " + amount);
         return false;
     }
+
 }
---
+++ core/src/ru/geekbrains/base/Sprite.java
@@ -0,0 +1,67 @@
+package ru.geekbrains.base;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
+
+import ru.geekbrains.math.Rect;
+
+public abstract class Sprite extends Rect {
+
+    protected float angle;
+    protected float scale = 1f;
+    protected TextureRegion[] regions;
+    protected int frame;
+
+    public Sprite(TextureRegion region) {
+        if (region == null){
+            throw new RuntimeException("Не задана текстура");
+        }
+        regions = new TextureRegion[1];
+        regions[0] = region;
+    }
+
+    public void drow(SpriteBatch batch){
+        batch.draw(
+                regions[frame],
+                getLeft(), getBottom(),  // начальная позиция
+                halfWidth, halfHeight,   // точка вращения
+                getWidth(), getHeight(), // ширина/высота
+                scale, scale,            // скалирование по осям
+                angle                    // угол поворота
+        );
+    }
+
+    public void setHeightProportion(float height){
+        setHeight(height);
+        float aspect = regions[frame].getRegionWidth() / (float) regions[frame].getRegionHeight();
+        setWidth(height * aspect);
+
+
+    }
+
+    public void resize(Rect worldBuonds){};
+
+    public void touchDown(Vector2 touch, int pointer, int button){};
+
+    // add same contructions for touchUp/touchDragged
+
+    public float getAngle() {
+        return angle;
+    }
+
+    public void setAngle(float angle) {
+        this.angle = angle;
+    }
+
+    public float getScale() {
+        return scale;
+    }
+
+    public void setScale(float scale) {
+        this.scale = scale;
+    }
+
+    public abstract void  update(float delta);
+
+}
