c685640fd779b65159e0a446fdf6c82128f2b336
Grigoriy <arcebus84@yandex.ru>
2020-03-07 22:21:22 +0300

    Lesson5

--- core/src/ru/geekbrains/base/Ship.java
+++ core/src/ru/geekbrains/base/Ship.java
@@ -1,5 +1,6 @@
 package ru.geekbrains.base;
 
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
@@ -8,10 +9,13 @@ public abstract class Ship extends Sprite {
     protected static final float V_LEN = 0.01f;
 
     protected Vector2 v;
+    protected Vector2 v0 = new Vector2(0.01f, 0);
+    protected Sound bulletSound;
 
-    public Ship(TextureRegion _region) {
-        super(_region);
+    public Ship(TextureRegion _region, int _row, int _col, int _frames, Sound _bulletSound) {
+        super(_region, _row, _col, _frames);
         this.v = new Vector2();
+        this.bulletSound = _bulletSound;
     }
 
     @Override
---
+++ core/src/ru/geekbrains/base/SpritesPool.java
@@ -0,0 +1,69 @@
+package ru.geekbrains.base;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class SpritesPool<T extends Sprite> {
+
+    private final List<T> activeObjects = new ArrayList<>();
+    private final List<T> freeObjects   = new ArrayList<>();
+
+    protected abstract T newObject();
+
+    public T obtain(){
+        T object;
+        if (freeObjects.isEmpty()){
+            object = newObject();
+        }else{
+            object = freeObjects.remove(freeObjects.size() - 1);
+        }
+
+        activeObjects.add(object);
+
+        return object;
+    }
+
+    public void updateActiveSprites(float delta){
+        for(Sprite sprite : activeObjects){
+            if(!sprite.isDestroyed){
+                sprite.update(delta);
+            }
+        }
+    }
+
+    public void drowAcriveObjects(SpriteBatch _b){
+        for(Sprite sprite : activeObjects){
+            if(!sprite.isDestroyed){
+                sprite.drow(_b);
+            }
+        }
+    }
+
+    private void free(T _object){
+        if(activeObjects.remove(_object)){
+            freeObjects.add(_object);
+        }
+    }
+
+    public void freeAllDestroyedActiveObjects(){
+        for(int i =0; i < activeObjects.size(); i++){
+            T sprite = activeObjects.get(i);
+            if(sprite.isDestroyed){
+                free(sprite);
+                i--;
+                sprite.flushDestroy();
+            }
+        }
+    }
+
+    public List<T> getActiveObjects() {
+        return activeObjects;
+    }
+
+    public void dispose(){
+        activeObjects.clear();
+        freeObjects.clear();
+    }
+}
